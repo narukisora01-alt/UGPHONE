141387a0cdb4597563ea61b852c17bb4fad2309e
TKScripts <narukisora01@gmail.com>
2026-01-02 01:13:04 +0800

    Update player.js

--- api/player.js
+++ api/player.js
@@ -2,7 +2,6 @@
 const players = global.players || (global.players = {});
 
 export default function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Roblox-Id');
